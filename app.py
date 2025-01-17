9644e6459d95e1935642472c4405b9c112c9af90
digvijay-21 <150232110+digvijay-21@users.noreply.github.com>
2025-01-17 10:32:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,11 +35,11 @@ def load_saved_artifacts():
     global __locations
     global __model
 
-    with open("./artifacts/columns.json", 'r') as f:
+    with open("columns.json", 'r') as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
 
-    with open("./artifacts/pune_home_prices_model.pickle", 'rb') as f:
+    with open("pune_home_prices_model.pickle", 'rb') as f:
         __model = pickle.load(f)
 
 
